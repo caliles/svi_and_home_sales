6ee9e1d809dee8823f5f059dbc0989e75e97621b
caliles <c.l1010@yahoo.com>
2024-06-29 08:27:19 -0400

    Functionalized on command line and removed unneeded libraries.

--- data_download_utilities.py
+++ data_download_utilities.py
@@ -4,38 +4,23 @@ Created on Jun 28, 2024
 @author: cl101
 '''
 
-import requests
-from svi_data import svi
-print(requests.certs.where())
-import json
-import csv
+import sys
 import pandas as pd
 
-def zillow_download():
+def csv_download(url):
     '''
     TODO
     '''
-    url = 'https://files.zillowstatic.com/research/public_csvs/zhvi/County_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv?t=1719616688'
     df = pd.read_csv(url)
-    return df
-    
-        
-
-def svi_download():
-    '''
-    TODO
-    '''
-    url = 'https://data.cdc.gov/api/views/48va-t53r/rows.csv'
-    df = pd.read_csv(url)
-    return df
-    
+    return df  
 
 
-def main():
-    zillow_df = zillow_download()
+def main(url):
+    print(url)
+    zillow_df = csv_download(url)
     print(zillow_df)
-    svi_df = svi_download()
-    print(svi_df)
+
 
 if __name__ == '__main__':
-    main()
+    
+    main(sys.argv[1])
