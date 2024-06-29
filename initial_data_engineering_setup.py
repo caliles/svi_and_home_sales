5f9a2151a97335fd6c6f9fac929e47d6f5f6fbb2
caliles <c.l1010@yahoo.com>
2024-06-29 20:57:14 +0000

    Troubleshot and fixed initial table build in GCP.

--- initial_data_engineering_setup.py
+++ initial_data_engineering_setup.py
@@ -14,7 +14,12 @@ from data_download_utilities import csv_download, bq_download
 
 def pull_and_prep_zillow_data(dl_year, state_fips_code = "All"):
     '''
-    TODO
+    Pulls zillow data and preps it by averaging over the year of interest.  Also fixes issus with leading zeros being missing from FIPS codes.
+    Args:
+        dl_year: the year on which to subset the pulled data.
+        state_fips_code: the fips code for the state of interest if there is one.
+    Returns:
+        df_zillow: a pandas dataframe with the prepped data.
     '''
     df_zillow = csv_download('https://files.zillowstatic.com/research/public_csvs/zhvi/County_zhvi_uc_sfrcondo_tier_0.0_0.33_sm_sa_month.csv?t=1719616688')
     # We will fix cases where leading zeros were not kept for the state and municipal Federal Information Processing System (FIPS) codes.
@@ -23,17 +28,17 @@ def pull_and_prep_zillow_data(dl_year, state_fips_code = "All"):
     df_zillow['StateCodeFIPS'] = df_zillow['StateCodeFIPS'].apply('{:0>2}'.format)
     df_zillow['MunicipalCodeFIPS'] = df_zillow['MunicipalCodeFIPS'].apply('{:0>3}'.format)
     if state_fips_code != 'All':
-        zillow_st_df = df_zillow[df_zillow['StateCodeFIPS'] == state_fips_code]
-    zillow_st_df['county_fips_code'] =  zillow_st_df['StateCodeFIPS'] + zillow_st_df['MunicipalCodeFIPS']
+        df_zillow = df_zillow[df_zillow['StateCodeFIPS'] == state_fips_code]
+    df_zillow['county_fips_code'] =  df_zillow['StateCodeFIPS'] + df_zillow['MunicipalCodeFIPS']
     months_to_avg_list = []
-    for n in zillow_st_df.columns:
+    for n in df_zillow.columns:
         if n[0:4] == '2020':
             print(n)
             months_to_avg_list.append(n)
-    zillow_st_df['average'] = zillow_st_df[months_to_avg_list].mean(axis=1)
+    df_zillow['average'] = df_zillow[months_to_avg_list].mean(axis=1)
     # Need to drop State column to avoid a conflict in variable names when merging:
-    zillow_st_df.drop('State', axis = 1, inplace = True)
-    return zillow_st_df
+    df_zillow.drop('State', axis = 1, inplace = True)
+    return df_zillow
 
 
 def main(dl_year, state_fips_code, project_id, dataset_name, table_id):
@@ -77,14 +82,13 @@ def main(dl_year, state_fips_code, project_id, dataset_name, table_id):
     dataset = client.create_dataset(dataset, timeout=30, exists_ok=True)  # If the dataset exists from a previous run of this code, this should not error out.
     print("Created dataset {}.{}".format(client.project, dataset.dataset_id))
 
-    # Now build a table and push the merged_df data into it.
-    table_id = 'merged_data'
+    # Now build a table and push the merged_df data into it.  This will throw an error if the table already exists.
     table_id_combined = f'{dataset_name}.{table_id}'
     pandas_gbq.to_gbq(merged_df, table_id_combined, project_id=project_id, table_schema=[{'name': 'county_geom', 'type': 'GEOGRAPHY'}])
     
 
 if __name__ == '__main__':
-    if len(sys.argv != 6):
-        print('Incorrect format, try again:\npython3 inititial_data_engineering_setup.py [dl_year], [state_fips_code], [project_id], [dataset_name], [table_id]')
+    if len(sys.argv) != 6:
+        print('Incorrect format, try again:\npython3 initial_data_engineering_setup.py [dl_year] [state_fips_code] [project_id] [dataset_name] [table_id]')
     else:
         main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
